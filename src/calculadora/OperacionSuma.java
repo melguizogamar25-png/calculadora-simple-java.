43bf8d5fb63b65259e1f3bd02ae25fb3174b173f
Marta Melguizo García <melguizo.gamar25@triana.salesianos.edu>
2025-11-17 22:48:35 +0100

    Versión inicial

---
+++ src/calculadora/OperacionSuma.java
@@ -0,0 +1,8 @@
+package calculadora;
+
+public class OperacionSuma {
+
+	 public static int sumar(int a, int b) {
+	        return a + b;
+	    }
+}
---
+++ src/calculadora/Principal.java
@@ -0,0 +1,12 @@
+package calculadora;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int resultado = OperacionSuma.sumar(5, 3);
+        System.out.println("El resultado de la suma es: " + resultado);
+	}
+
+}
