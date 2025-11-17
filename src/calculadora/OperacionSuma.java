80eae3456a4e99050acf04856e0c5ad39b634ac0
Marta Melguizo García <melguizo.gamar25@triana.salesianos.edu>
2025-11-17 22:54:12 +0100

    Versión nueva

--- src/calculadora/OperacionSuma.java
+++ src/calculadora/OperacionSuma.java
@@ -3,6 +3,7 @@ package calculadora;
 public class OperacionSuma {
 
 	 public static int sumar(int a, int b) {
+		 System.out.println("Realizando la operación de suma...");
 	        return a + b;
 	    }
 }
--- src/calculadora/Principal.java
+++ src/calculadora/Principal.java
@@ -5,6 +5,7 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		System.out.println("Bienvenido a la calculadora simple.");
 		int resultado = OperacionSuma.sumar(5, 3);
         System.out.println("El resultado de la suma es: " + resultado);
 	}
